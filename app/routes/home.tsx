0eaf23a392095b84722eb23240c0bb93c36d42b2
Phanna <helphanna610@gmail.com>
2026-01-17 00:23:39 +0700

    update home and about space, responsive

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -11,12 +11,12 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-    <div className="px-5 py-10 bg-white md:px-0">
+    <div className="px-5 py-5 bg-white sm:px-8 sm:py-12">
       <div className="container items-center max-w-6xl mx-auto xl:px-5">
         <div className="flex flex-wrap items-center sm:-mx-3">
           <div className="w-full md:w-1/2 md:px-3">
             <div className="space-y-6 sm:max-w-md lg:max-w-lg">
-              <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl">
+              <h1 className="font-extrabold tracking-tight text-gray-900 text-3xl sm:text-4xl lg:text-5xl">
                 <span className="block xl:inline">Explore Countries with </span>
                 <span className="block text-indigo-600 xl:inline">
                   Real-Time Data
@@ -26,10 +26,10 @@ export default function Home() {
                 Discover details about every country around the world – from
                 capitals to regions!
               </p>
-              <div className="flex flex-col sm:flex-row sm:space-x-4">
+              <div className="flex flex-col sm:flex-row">
                 <Link
                   to="/countries"
-                  className="flex items-center justify-center px-6 py-3 text-lg text-white bg-indigo-600 rounded-md hover:bg-indigo-700"
+                  className="flex items-center justify-center px-3 py-2 sm:px-6 sm:py-3 text-lg text-white bg-indigo-600 rounded-md hover:bg-indigo-700"
                 >
                   Explore Now
                   <svg
@@ -48,14 +48,14 @@ export default function Home() {
                 </Link>
                 <Link
                   to="/about"
-                  className="flex items-center px-6 py-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600"
+                  className="flex items-center justify-center mt-4 sm:mt-0 ml-4 px-3 py-2 sm:px-6 sm:py-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600"
                 >
                   Learn More
                 </Link>
               </div>
             </div>
           </div>
-          <div className="w-full md:w-1/2">
+          <div className="w-full md:w-1/2 mt-5 md:mt-0">
             <div className="overflow-hidden rounded-md shadow-xl">
               <img
                 src={AngkorWat}
